59a98e3c2018d8aeca5bd5b4912da62528bf3819
SashaGonchar <nekospoon228@gmail.com>
2025-03-09 22:17:58 +0200

    created skeleton for counter app

---
+++ src/components/app/app.js
@@ -0,0 +1,16 @@
+import './app.css'
+import '../window/window'
+import Window from "../window/window";
+
+function App () {
+
+    return (
+        <div className={"app"}>
+            <Window>
+
+            </Window>
+        </div>
+    );
+}
+
+export default App;
